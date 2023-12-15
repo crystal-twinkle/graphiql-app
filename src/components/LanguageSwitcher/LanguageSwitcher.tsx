1f8497e8be8a7aa923147858d072df3517cd4e9d
crystal-twinkle <autoro1@mail.ru>
2023-12-15 16:42:07 +0200

    refactor: add event delegation

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -12,9 +12,13 @@ const LanguageSwitcher = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
 
-  const handleLanguage = (languageCode: Language) => {
-    changeLanguage(languageCode);
-    toggleDropdown();
+  const handleLanguageClick = (event: React.MouseEvent<HTMLElement>) => {
+    const target = event.target as HTMLElement;
+    if (target.tagName === 'LI') {
+      const languageCode = target.dataset.code as Language;
+      changeLanguage(languageCode);
+      toggleDropdown();
+    }
   };
 
   const languages = [
@@ -28,13 +32,9 @@ const LanguageSwitcher = () => {
       <Button icon={localIcon} text={language} onclick={toggleDropdown} />
       {isDropdownOpen && (
         <div className="absolute bg-light p-2 cursor-default">
-          <ul className="flex flex-col items-center gap-2">
+          <ul className="flex flex-col items-center gap-2" onClick={handleLanguageClick}>
             {languages.map((language) => (
-              <li
-                className="cursor-pointer"
-                key={language.code}
-                onClick={() => handleLanguage(language.code as Language)}
-              >
+              <li className="cursor-pointer" key={language.code} data-code={language.code}>
                 {language.label}
               </li>
             ))}
