d865451787495bf123147b2ca1e7d2ac6e6bb041
SadJoeBright <sadjoebright.gmail.com>
2023-12-17 01:30:50 +0200

    feat: add cursor managment for variables and headers editor

--- src/components/VariableHeaderEditor/VariableHeaderEditor.tsx
+++ src/components/VariableHeaderEditor/VariableHeaderEditor.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import LineCounter from '../LineCounter/LineCounter';
 import Button from '../UI/Button';
 import chevronIcon from '../../assets/icons/chevron-icon.svg';
+import { manageCursor } from '../../utils/manageCursor';
 
 function VariableHeaderEditor() {
   enum Tabs {
@@ -12,7 +13,8 @@ function VariableHeaderEditor() {
 
   const [headersValue, setHeadersValue] = useState('');
   const [variablesValue, setVariablesValue] = useState('');
-  const [currentTab, setCurrentTab] = useState(Tabs.NONE);
+  const [activeTab, setActiveTab] = useState(Tabs.NONE);
+  const [isFocused, setIsFocused] = useState(true);
 
   const handleHeadersChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setHeadersValue(event.target.value);
@@ -26,39 +28,47 @@ function VariableHeaderEditor() {
     <>
       <div className="relative z-10 flex justify-between gap-10 px-5 pt-2 pb-0 border-t-2 bg-medium border-light">
         <div className="flex items-center gap-5">
-          <div className={`${currentTab === 'variables' && 'underline'}`}>
-            <Button onclick={() => setCurrentTab(Tabs.VARIABLES)} text="Variables" />
+          <div className={`${activeTab === 'variables' && 'underline'}`}>
+            <Button onclick={() => setActiveTab(Tabs.VARIABLES)} text="Variables" />
           </div>
 
-          <div className={` ${currentTab === 'headers' && 'underline'}`}>
-            <Button onclick={() => setCurrentTab(Tabs.HEADERS)} text="Headers" />
+          <div className={` ${activeTab === 'headers' && 'underline'}`}>
+            <Button onclick={() => setActiveTab(Tabs.HEADERS)} text="Headers" />
           </div>
         </div>
         <div
           className={`transition-all duration-500 ease-in-out ${
-            currentTab !== Tabs.NONE && 'rotate-180'
+            activeTab !== Tabs.NONE && 'rotate-180'
           }`}
         >
           <Button
-            onclick={() => setCurrentTab(currentTab === Tabs.NONE ? Tabs.VARIABLES : Tabs.NONE)}
+            onclick={() => setActiveTab(activeTab === Tabs.NONE ? Tabs.VARIABLES : Tabs.NONE)}
             icon={chevronIcon}
           />
         </div>
       </div>
       <div
-        className={`flex -my-6 scale-y-0 p-0 transition-scale duration-500 ease-in-out overflow-hidden ${
-          currentTab !== Tabs.NONE && '-my-0 scale-y-100'
+        className={`flex scale-y-0 -my-6 p-0 transition-scale duration-500 ease-in-out overflow-hidden ${
+          activeTab !== Tabs.NONE && 'my-0 scale-y-100'
         }`}
       >
-        <div className={`${currentTab === Tabs.NONE && 'h-0'}`}>
-          <LineCounter value={currentTab === Tabs.HEADERS ? headersValue : variablesValue} />
+        <div className={`${activeTab === Tabs.NONE && 'h-0 overflow-hidden'}`}>
+          <LineCounter value={activeTab === Tabs.HEADERS ? headersValue : variablesValue} />
         </div>
         <textarea
           autoFocus
-          onChange={currentTab === Tabs.HEADERS ? handleHeadersChange : handleVariablesChange}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          onChange={activeTab === Tabs.HEADERS ? handleHeadersChange : handleVariablesChange}
+          onKeyDown={(event) =>
+            manageCursor(
+              event,
+              isFocused,
+              activeTab === Tabs.HEADERS ? setHeadersValue : setVariablesValue
+            )
+          }
           name="editor"
-          placeholder={currentTab}
-          value={currentTab === Tabs.HEADERS ? headersValue : variablesValue}
+          value={activeTab === Tabs.HEADERS ? headersValue : variablesValue}
           className="w-full px-2 overflow-hidden bg-medium outline-none resize-none"
         ></textarea>
       </div>
