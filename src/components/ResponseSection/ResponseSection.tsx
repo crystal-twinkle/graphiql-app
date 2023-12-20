68ef070bf7275ab0a3c20d5a39335fe02caa141f
SadJoeBright <sadjoebright.gmail.com>
2023-12-20 17:59:04 +0200

    feat: implement sending headers to server

--- src/components/ResponseSection/ResponseSection.tsx
+++ src/components/ResponseSection/ResponseSection.tsx
@@ -5,7 +5,9 @@ function ResponseSection() {
   const result = useSelector((state: RootState) => state.result.result);
 
   return (
-    <section className="max-h-screen overflow-auto w-1/2 p-5 whitespace-pre-wrap">{result}</section>
+    <section className="max-h-screen overflow-auto w-1/2 px-5 py-1 whitespace-pre-wrap font-mono">
+      {result}
+    </section>
   );
 }
 
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { userSlice } from './user-slice';
 import { endpointReducer } from './endpoint-slice';
 import { resultReducer } from './result-slice';
 import { variablesReducer } from './variables-slice';
+import { headersReducer } from './headers-slice';
 
 export const rootReducer = combineReducers({
-  user: userSlice.reducer,
   endpoint: endpointReducer,
   result: resultReducer,
   variables: variablesReducer,
+  headers: headersReducer,
 });
 
 export const setupStore = () => {
