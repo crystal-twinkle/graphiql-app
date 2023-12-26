fb9f03fa37990497ee62797f29cc7c5f16a87b89
crystal-twinkle <autoro1@mail.ru>
2023-12-26 13:36:41 +0200

    feat: add sign-up test

---
+++ src/__tests__/pages/sign-up.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {renderWithProviders} from '../tests-utils/renderWithProviders';
+import {SignUpPage} from '../../pages/Sign-up-page';
+
+describe('Sign Up page test', () => {
+  it('Render Sign Up component', () => {
+    renderWithProviders(<SignUpPage/>)
+  });
+});
